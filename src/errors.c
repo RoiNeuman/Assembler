ffafe064b3f9c013335acdb2e2ec776a850d564b
Roi <roi.neuman@gmail.com>
2018-03-06 00:05:24 +0200

    Improved the error handling.

--- src/errors.c
+++ src/errors.c
@@ -1,18 +1,83 @@
 #include <stdio.h>
 #include "errors.h"
+#include "memoryManager.h"
 
-void printError(Error e)
+static ErrorNode *head; /* The error list head pointer */
+
+/* Filter and print single error */
+void printError(Error e, const char *msg)
 {
-    /* Filter and print the error */
     switch (e) {
         case outOfMemory:
-            fprintf(stderr, "Error: out of memory.\n");
+            if (msg != NULL) {
+                fprintf(stderr, "Error: out of memory. ");
+                fprintf(stderr, msg);
+                fprintf(stderr, "\n");
+            } else {
+                fprintf(stderr, "Error: out of memory.\n");
+            }
             break;
         case fileNotExist:
-            fprintf(stderr, "Error: file not exist.\n");
+            if (msg != NULL) {
+                fprintf(stderr, "Error: file not exist. ");
+                fprintf(stderr, msg);
+                fprintf(stderr, "\n");
+            } else {
+                fprintf(stderr, "Error: file not exist.\n");
+            }
             break;
         case none:
         default:
             break;
+    }
+}
+
+/* Printing all errors */
+void printAllErrors()
+{
+    ErrorNode *pn;
+    pn = head;
+
+    while (pn != NULL) {
+        printError(pn->error, pn->msg);
+        pn = pn->next;
+    }
+}
+
+/* Log a new error into the error list */
+void logError(Error e, const char *msg)
+{
+    ErrorNode *newError;
+    ErrorNode *pn;
+    ErrorNode *prev;
+
+    /* Allocating memory for the new error */
+    newError = (ErrorNode *)autoDispMalloc(sizeof(ErrorNode));
+
+    if (newError == NULL) {
+        /* Critical out of memory error, unable to log any more errors */
+        printAllErrors();
+        printError(e, msg);
+        printError(outOfMemory, "Error handler, logError().");
+        freeAll();
+        exit(1);
+    }
+
+    newError->error = e;
+    newError->msg = msg;
+    newError->next = NULL;
+
+    if (head == NULL) {
+        /* First error in the error list */
+        head = newError;
+    } else {
+        /* Adding the new error to the end of the error list */
+        prev = head;
+        pn = head->next;
+        while (pn != NULL) {
+            prev = pn;
+            pn = pn->next;
+        }
+        prev->next = newError;
     }
 }
--- src/errors.h
+++ src/errors.h
@@ -1,12 +1,27 @@
 #ifndef ASSEMBLER_ERRORS_H
 #define ASSEMBLER_ERRORS_H
 
+/* All error types */
 typedef enum Error {
     none,
     outOfMemory,
     fileNotExist
 } Error;
 
-void printError(Error e);
+/* A node in the error list */
+typedef struct ErrorNode {
+    Error error; /* The relevant error */
+    const char *msg; /* possible message */
+    struct ErrorNode *next; /* The next error in the list */
+} ErrorNode;
+
+/* Filter and print single error */
+void printError(Error e, const char *msg);
+
+/* Printing all errors */
+void printAllErrors();
+
+/* Log a new error into the error list */
+void logError(Error e, const char *msg);
 
 #endif
